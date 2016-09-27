392ce66db0f28e09efe0677646dee36c531e367c
ChuGuangFu <1030769453@qq.com>
2016-09-27 14:58:25 +0800

    注释

---
+++ Swift_note/Swift_note/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Swift_note
+//
+//  Created by 梁羽 on 16/9/27.
+//  Copyright © 2016年 梁羽. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+//MARK: 同步笔记
+
